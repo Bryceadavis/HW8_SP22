f91c4a437aa38d873b3ba0548df5f83d54a8b2a2
Bryce D <102119818+Bryceadavis@users.noreply.github.com>
2022-04-01 14:38:44 -0500

    Update Rankine_app_MVC.py

--- Rankine_app_MVC.py
+++ Rankine_app_MVC.py
@@ -3,6 +3,10 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from Rankine_GUI import Ui_Form
 from Rankine_Classes import rankineController
+import Calc_state
+from Calc_state import SatPropsIsobar
+from Calc_state import UnitConverter as UC
+from Rankine_Classes import rankineView
 
 #these imports are necessary for drawing a matplot lib graph on my GUI
 #no simple widget for this exists in QT Designer, so I have to add the widget in code.
@@ -21,9 +25,9 @@ class MainWindow(qtw.QWidget, Ui_Form):
         self.MakeCanvas()
         #A tuple containing the widgets that get updated in the View
         self.widgets = (self.le_H1, self.le_H2, self.le_H3, self.le_H4, self.le_TurbineWork, self.le_PumpWork, self.le_HeatAdded, self.le_Efficiency, self.lbl_SatPropHigh, self.lbl_SatPropLow, self.ax, self.canvas)
-        self.RC=rankineController(self.widgets)  # instantiate a rankineController object
-
+        self.RC = rankineController(self.widgets)  # instantiate a rankineController object
         # a place to store coordinates from last position on graph
+        self.otherwidgets = (self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.lbl_PHigh, self.lbl_PLow, self.lbl_H1Units, self.lbl_H2Units, self.lbl_H3Units, self.lbl_H4Units, self.lbl_TurbineWorkUnits, self.lbl_PumpWorkUnits, self.lbl_HeatAddedUnits)
         self.oldXData=0.0
         self.oldYData=0.0
         # End main ui code
@@ -37,7 +41,9 @@ class MainWindow(qtw.QWidget, Ui_Form):
         self.btn_Calculate.clicked.connect(self.Calculate)
         self.rdo_Quality.clicked.connect(self.SelectQualityOrTHigh)
         self.rdo_THigh.clicked.connect(self.SelectQualityOrTHigh)
-        self.le_PHigh
+        self.le_PLow.textChanged[str].connect(self.Change)
+        self.le_PHigh.textChanged[str].connect(self.Changeh)
+        self.le_TurbineInletCondition.textChanged[str].connect(self.ChangeT)
 
     def MakeCanvas(self):
         """
@@ -49,13 +55,13 @@ class MainWindow(qtw.QWidget, Ui_Form):
         :return:
         """
         #Step 1.
-        self.figure=Figure(figsize=(2,4),tight_layout=True, frameon=True)
+        self.figure=Figure(figsize=(4,4),tight_layout=True, frameon=True)
         #Step 2.
         self.canvas=FigureCanvasQTAgg(self.figure)
         #Step 3.
         self.ax = self.figure.add_subplot()
-        #Step 4.
-        self.gb_Output.layout().addWidget(self.canvas, 6,0,1,6)
+        #Step 4. I can not here you sorry. I had gotten off the call
+        self.layoutHorizontalMain.addWidget(self.canvas)
         self.canvas.mpl_connect("motion_notify_event", self.mouseMoveEvent)
 
     #since my main window is a widget, I can customize its events by overriding the default event
@@ -64,12 +70,31 @@ class MainWindow(qtw.QWidget, Ui_Form):
         self.oldYData=event.ydata if event.ydata is not None else self.oldYData
         self.setWindowTitle('s:{:0.2f} kJ/(kg*K), T:{:0.2f} C'.format(self.oldXData, self.oldYData))
 
+
+    def SelectQualityOrTHigh(self):
+        self.Tclick = SatPropsIsobar(P=int(self.le_PHigh.text())*100)
+        self.lbl_TurbineInletCondition.setText(("Turbine Inlet: {} =".format('x'if self.rdo_Quality.isChecked() else 'THigh')))
+        if self.rdo_THigh.isChecked():
+            self.le_TurbineInletCondition.setText(str(self.Tclick.TSat))
+
+
     def Calculate(self):
         #use rankineController to update the model based on user inputs
         self.RC.updateModel((self.le_PHigh, self.le_PLow, self.rdo_Quality, self.le_TurbineInletCondition, self.le_TurbineEff))
 
-    def SelectQualityOrTHigh(self):
-        self.lbl_TurbineInletCondition.setText(("Turbine Inlet: {} =".format('x'if self.rdo_Quality.isChecked() else 'THigh')))
+    def Change(self):
+
+        self.RC.updateModel((self.le_PHigh, self.le_PLow, self.rdo_Quality, self.le_TurbineInletCondition, self.le_TurbineEff))
+        self.show()
+
+    def Changeh(self):
+        self.RC.updateModel((self.le_PHigh, self.le_PLow, self.rdo_Quality, self.le_TurbineInletCondition, self.le_TurbineEff))
+        self.show()
+    def ChangeT(self):
+        self.RC.updateModel((self.le_PHigh, self.le_PLow, self.rdo_Quality, self.le_TurbineInletCondition, self.le_TurbineEff))
+        self.show()
+
+
 
 #if this module is being imported, this won't run. If it is the main module, it will run.
 if __name__== '__main__':
