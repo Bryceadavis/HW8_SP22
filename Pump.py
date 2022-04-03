0a5074e22f85980b0830d102418eecef4e9abee1
AbbyOakes7 <100322867+AbbyOakes7@users.noreply.github.com>
2022-04-02 23:28:59 -0500

    Abby-reads pump1.txt

--- Pump.py
+++ Pump.py
@@ -2,15 +2,18 @@ import numpy as np
 from Calc_state import Steam_SI as steam  # import any of your own classes as you wish
 
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtWidgets import QFileDialog, QMessageBox
-from PyQt5.QtGui import QCursor
-from PyQt5.QtCore import Qt
+#from PyQt5.QtWidgets import QWidget, QApplication
+# from PyQt5.QtWidgets import QFileDialog, QMessageBox
+# from PyQt5.QtGui import QCursor
+# from PyQt5.QtCore import Qt
 
 from Pump_GUI import Ui_Form  # from the GUI file your created
 
+import PyQt5.QtWidgets as qtw
+import PyQt5.QtCore as qtc
+import PyQt5.QtGui as qtg
 
-class main_window(QWidget, Ui_Form):
+class main_window(qtw.QWidget, Ui_Form):
     def __init__(self):
         """
         Constructor for the main window of the application.  This class inherits from QWidget and Ui_Form
@@ -26,47 +29,31 @@ class main_window(QWidget, Ui_Form):
     def assign_widgets(self):
         """This function controls what happens when the GUI buttons are clicked."""
         self.ExitPushButton.clicked.connect(self.ExitApp) #closes app when exit is clicked
-        #self.FilenamePushButton.clicked.connect(self.QFileDialog) #calls Calculate function when Calculate is clicked
+        # dialog= QFileDialog(self)
+        # dialog.setFileMode(QFileDialog.AnyFile)
+        self.FilenamePushButton.clicked.connect(self.OpenDialog) #calls Calculate function when Calculate is clicked
 
-    def Calculate(self):
+    def OpenDialog(self):  #borrowed from Dr.Smay's PipeNetwork_App
         """
-        Here, we need to scan through the check boxes and ensure that only two are selected and define properties
-        for calculating the state of the steam.  Then set the properties of the steam object and calculate the
-        steam state.  Finally, output the results to the line edit widgets.
+        Read the information from a pipe network file.
         :return:
         """
-        # make sure only two boxes checked
-        nChecked = 0
-        for c in self.checkBoxes:
-            nChecked += 1 if c.isChecked() else 0
-        if nChecked != 2:
+        # open the file dialog box to search for the file I want
+        filename = qtw.QFileDialog.getOpenFileName()[0]
+        if len(filename) == 0:  # no file selected
             return
-
-        self.Steam.P = float(self.le_P.text()) if self.chk_Press.isChecked() else None #converts typed number to float and assigns to steam parameter if box is checked
-        self.Steam.T = float(self.le_T.text()) if self.chk_Temp.isChecked() else None
-        self.Steam.x = float(self.le_Q.text()) if self.chk_Quality.isChecked() else None
-        self.Steam.h = float(self.le_H.text()) if self.chk_Enthalpy.isChecked() else None
-        self.Steam.s = float(self.le_S.text()) if self.chk_Entropy.isChecked() else None
-        self.Steam.v = float(self.le_SpV.text()) if self.chk_SpV.isChecked() else None
-
-        self.Steam.calc()
-        state = self.Steam #not used
-        self.le_P.setText("{:.2f}".format(self.Steam.P)) #&AKO altered to match HW doc decimal places
-        self.le_T.setText("{:.2f}".format(self.Steam.T)) #edits line outputs;prints what you input as well as what is calculated from your inputs to GUI
-        self.le_Q.setText("{:.4f}".format(self.Steam.x))
-        self.le_H.setText("{:.2f}".format(self.Steam.h))
-        self.le_S.setText("{:.4f}".format(self.Steam.s))
-        self.le_SpV.setText("{:.5f}".format(self.Steam.v))
-        self.lbl_Properties.setText((str(self.Steam.region)))
-        return
+        self.le_Filename.setText(filename)  # echo the filename on the GUI
+        file = open(filename, 'r')  # open the file
+        data = file.readlines()  # read all the lines of the file into a list of strings
+        # self.Controller.importPipeNetwork(data, PN=self.Model)  # import the pipe network information
+        # self.updateView()  # update the view of the model
+        pass
 
     def ExitApp(self):
         app.exit()
 
-if __name__ == "__main__":
-    app = QApplication.instance()
-    if not app:
-        app = QApplication(sys.argv)
-    app.aboutToQuit.connect(app.deleteLater)
-    main_win = main_window()
-    sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = main_window()
+    mw.setWindowTitle('Pipe Network Designer')
+    sys.exit(app.exec())
